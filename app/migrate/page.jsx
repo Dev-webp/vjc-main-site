1ebc85d31cac3c00ffaafd659fa68ae9efb36f0e
Dev-webp <srichain18@gmail.com>
2025-02-13 18:32:54 +0530

    Decsribe

--- app/migrate/page.jsx
+++ app/migrate/page.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import Nav from "./Nav";
 import Form from "./Form";
-import Two from "./two";
+import Two from "./Two";
 import Footer from "./Footer";
 
 const textContainerVariants = {
